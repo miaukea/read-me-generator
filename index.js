2106b284cea6ac17d0115b8bd2f3f253c393263f
Melody <meliaukea@gmail.com>
2025-01-05 11:33:28 -0500

    Added code that will create the READ-ME file after the prompt is filled out

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirier';
+import fs from 'fs';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+        .prompt ([
+            {
+                type: 'input',
+                message: 'What is the project title?',
+                name: 'title'
+            },
+            {
+                type: 'input',
+                message: 'Please write a brief description for the project:',
+                name: 'description'
+            },
+            {
+                type: 'input',
+                message: 'Please write installation instructions for the project:',
+                name: 'instructions'
+            },
+            {
+                type: 'input',
+                message: 'How would you like the project to be used?',
+                name: 'usage'
+            },
+            {
+                type: 'input',
+                message: 'Who are the contributors of the project?',
+                name: 'contributing'
+            },
+            {
+                type: 'input',
+                message: 'Please include the test instructions for the project:',
+                name: 'test'
+            }
+        ]).then((response) => {
+            const readmeContent = writeToFile(response);
+
+            fs.appendFile('README.md', readmeContent, (err) => {
+                if (err) throw err;
+                console.log('Your READ-ME was successfully made');
+            })
+        })
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = ({title, description, instructions, usage, contributing, test}) => {
+    `# ${title}
+
+    ## Description
+    ${description}
+
+    ## Installation
+    ${instructions}
+
+    ## Usage
+    ${usage}
+
+    ## Contributing
+    ${contributing}
+
+    ## Tests
+    ${test}
+    `;
+}
 
 // TODO: Create a function to initialize app
 function init() {}
